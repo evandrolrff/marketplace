794acf5b93da83f7d38548b2ffbc56bbded4a4da
Evandro <evandrolrff@gmail.com>
2024-03-18 20:29:21 -0300

    sketch database

---
+++ database.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE ecommerce;
+
+USE ecommerce;
+
+CREATE TABLE Product (
+    product_id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL,
+    rate FLOAT NOT NULL,
+    description VARCHAR(255) NULL,
+    PRIMARY KEY (product_id));
+
+CREATE TABLE User (
+    user_id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM("Admin", "Customer", "Nonexistent") NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id));
+
+CREATE TABLE Comment (
+    comment_id INTEGER NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    description VARCHAR(255) NULL,
+    rate FLOAT NOT NULL,
+    PRIMARY KEY(comment_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (product_id) REFERENCES Product(product_id));
