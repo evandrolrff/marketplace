36dd999c1ff1bc839e1bfdca396a906b7980bdf8
Evandro <evandrolrff@gmail.com>
2024-03-21 21:55:13 -0300

    added new attributes to db

--- database.sql
+++ database.sql
@@ -4,17 +4,19 @@ USE ecommerce;
 
 CREATE TABLE Product (
     product_id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name_product VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
     rate FLOAT NOT NULL,
-    description VARCHAR(255) NULL,
+    description_product VARCHAR(255) NULL,
+    image_path VARCHAR(255) NULL,
     PRIMARY KEY (product_id));
 
 CREATE TABLE User (
     user_id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM("Admin", "Customer", "Nonexistent") NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    name_user VARCHAR(255) NOT NULL,
+    password_user VARCHAR(255) NOT NULL,
+    role_user ENUM("Admin", "Customer", "Nonexistent") NOT NULL,
     email VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id));
 
@@ -22,7 +24,7 @@ CREATE TABLE Comment (
     comment_id INTEGER NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     product_id INT NOT NULL,
-    description VARCHAR(255) NULL,
+    description_comment VARCHAR(255) NULL,
     rate FLOAT NOT NULL,
     PRIMARY KEY(comment_id),
     FOREIGN KEY (user_id) REFERENCES User(user_id),
